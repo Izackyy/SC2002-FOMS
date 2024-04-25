ceeb89f25f44d881e208220b9ca5c45c325b9c1f
Izackyy <isaac.wong90@gmail.com>
2024-04-25 17:51:47 +0800

    updated adminController (jamie)

--- SC2002FOMS/src/Controllers/AdminController.java
+++ SC2002FOMS/src/Controllers/AdminController.java
@@ -64,11 +64,11 @@ public class AdminController extends EmployeeController {
 					setBranchStatus();
 					break;
 				case (7):
-					sc.nextLine();//input buffer
+					sc.nextLine();// input buffer
 					addBranch();
 					break;
 				case (8):
-					sc.nextLine();//input buffer
+					sc.nextLine();// input buffer
 					removeBranch();
 					break;
 				case (9):
@@ -82,137 +82,119 @@ public class AdminController extends EmployeeController {
 
 	private static void editStaffAcc() throws IOException {
 		// System.out.println("editing staff account");
-		
-		String yesNo; 
+
+		String yesNo;
 		System.out.println("===========Staff Editor==========");
 		System.out.println("|| 1) Add Staff                ||");
 		System.out.println("|| 2) Remove Staff             ||");
 		System.out.println("|| 3) Edit Staff               ||");
 		System.out.println("=================================");
-		
+
 		int editChoice = sc.nextInt();
-		sc.nextLine(); //input buffer
-		switch (editChoice)
-		{
-			case(1):
+		sc.nextLine(); // input buffer
+		switch (editChoice) {
+			case (1):
 				System.out.println("Name:");
 				String name = sc.nextLine();
-				
-				List<Staff> al = StaffTextDB.readStaff("staff.txt");//test
-		        for (Staff staff : al)
-		        {
-		        	if (staff.getName().equalsIgnoreCase(name))
-		        	{
-		        		System.out.println("Staff already exists");
-		        		return;
-		        	}
-		        }
-		       
+
+				List<Staff> al = StaffTextDB.readStaff("staff.txt");// test
+				for (Staff staff : al) {
+					if (staff.getName().equalsIgnoreCase(name)) {
+						System.out.println("Staff already exists");
+						return;
+					}
+				}
+
 				System.out.println("StaffID:");
 				String staffID = sc.nextLine();
 				System.out.println("Role: ");
 				String sRole = sc.nextLine();
-				
+
 				Role role = null;
-				if (sRole.equalsIgnoreCase("M"))
-				{
+				if (sRole.equalsIgnoreCase("M")) {
 					role = Role.M;
-				}
-				else if(sRole.equalsIgnoreCase("S"))
-				{
+				} else if (sRole.equalsIgnoreCase("S")) {
 					role = Role.S;
-				}
-				else
-				{
+				} else {
 					role = Role.A;
 				}
 				System.out.println("Gender: ");
 				String sGender = sc.nextLine();
 				Gender gender = null;
-				if(sGender.equalsIgnoreCase("F"))
-				{
+				if (sGender.equalsIgnoreCase("F")) {
 					gender = Gender.F;
-				}
-				else
-				{
+				} else {
 					gender = Gender.M;
 				}
 				System.out.println("Age:");
 				int age = sc.nextInt();
-				
-				int i=1;
+
+				int i = 1;
 				System.out.println("Branch:");
 				System.out.println("======Branch Option======");
-				List<Branch> b = BranchTextDB.readBranchList("branch.txt");//test
-		        for (Branch branch : b)
-		        {	
-		        	System.out.println(i + ") " + branch.getName());
-		            i++;
-		        }
-		        
-		        int selection = sc.nextInt();
-		        
-		        Branch branch = b.get(selection-1);
-				
-				
+				List<Branch> b = BranchTextDB.readBranchList("branch.txt");// test
+				for (Branch branch : b) {
+					System.out.println(i + ") " + branch.getName());
+					i++;
+				}
+
+				int selection = sc.nextInt();
+
+				Branch branch = b.get(selection - 1);
+
 				System.out.println("Staff has been successfully added");
 				Staff staff = new Staff(name, staffID, role, gender, age, branch.getName());
-			
+
 				StaffTextDB.addStaff("staff.txt", staff);
 				break;
-			
-			case(2):
-			{
+
+			case (2): {
 				StaffTextDB.printStaffList("staff.txt");
 				System.out.println("Enter Staff Name:");
 				String sName = sc.nextLine();
-				
-				List<Staff> s = StaffTextDB.readStaff("staff.txt");//test
+
+				List<Staff> s = StaffTextDB.readStaff("staff.txt");// test
 				Staff toRemove = null;
-		        for (Staff sf : s)
-		        {
-		        	if (sf.getName().equals(sName))
-		        	{
-		        		toRemove = sf;
-		        		StaffTextDB.removeStaff("staff.txt", toRemove);
-		        		System.out.println("Staff removed successfully.");
-		        		break;
-		        	}
-		        }
-		        if (toRemove == null)
-		        {
-		        	System.out.println("Staff does not exist");
-		        }
+				for (Staff sf : s) {
+					if (sf.getName().equals(sName)) {
+						toRemove = sf;
+						StaffTextDB.removeStaff("staff.txt", toRemove);
+						System.out.println("Staff removed successfully.");
+						break;
+					}
+				}
+				if (toRemove == null) {
+					System.out.println("Staff does not exist");
+				}
 				break;
 			}
-			case(3):
-			{ //loginID, password,age
+			case (3): { // loginID, password,age
 				Staff oldStaff = null;
 				System.out.println("========Staff list========");
 				StaffTextDB.printStaffList("staff.txt");
-				
-				List<Staff> s = StaffTextDB.readStaff("staff.txt");//test
-				
+
+				List<Staff> s = StaffTextDB.readStaff("staff.txt");// test
+
 				System.out.println("Select a staff member");
 				int choice = sc.nextInt();
 				oldStaff = s.get(choice - 1);
-				
-		        sc.nextLine(); //test input buffer
-		        
-		        System.out.println("Current details of selected Staff:");
-		        
-		        System.out.println("Name: " + oldStaff.getName());
-		        System.out.println("StaffID: " + oldStaff.getLoginID());
-		        System.out.println("Password: " + oldStaff.getPassword());
-		        System.out.println("Age: " + oldStaff.getAge());
-		        System.out.println("");
-				
-				Staff newStaff = new Staff(oldStaff.getName(), oldStaff.getLoginID(), oldStaff.getRole(), oldStaff.getGender(), oldStaff.getAge(), oldStaff.getBranch(),oldStaff.getPassword());
-				
+
+				sc.nextLine(); // test input buffer
+
+				System.out.println("Current details of selected Staff:");
+
+				System.out.println("Name: " + oldStaff.getName());
+				System.out.println("StaffID: " + oldStaff.getLoginID());
+				System.out.println("Password: " + oldStaff.getPassword());
+				System.out.println("Age: " + oldStaff.getAge());
+				System.out.println("");
+
+				Staff newStaff = new Staff(oldStaff.getName(), oldStaff.getLoginID(), oldStaff.getRole(),
+						oldStaff.getGender(), oldStaff.getAge(), oldStaff.getBranch(), oldStaff.getPassword());
+
 				System.out.println("Update StaffID? (Y/N)");
 				yesNo = sc.nextLine();
-				if (yesNo.equalsIgnoreCase("Y"))
-				{
+				if (yesNo.equalsIgnoreCase("Y")) {
 					System.out.println("New Staff ID:");
 					String newID = sc.nextLine();
 					newStaff.setLoginID(newID);
@@ -220,24 +202,22 @@ public class AdminController extends EmployeeController {
 				}
 				System.out.println("Update Password? (Y/N)");
 				yesNo = sc.nextLine();
-				if (yesNo.equalsIgnoreCase("Y"))
-				{
+				if (yesNo.equalsIgnoreCase("Y")) {
 					System.out.println("New password:");
 					String newPassword = sc.nextLine();
 					newStaff.setPassword(newPassword);
 					yesNo = "N";
 				}
-				
+
 				System.out.println("Update Age? (Y/N)");
 				yesNo = sc.nextLine();
-				if (yesNo.equalsIgnoreCase("Y"))
-				{
+				if (yesNo.equalsIgnoreCase("Y")) {
 					System.out.println("New age:");
 					int newAge = sc.nextInt();
 					newStaff.setAge(newAge);
 					yesNo = "N";
 				}
-				
+
 				StaffTextDB.updateStaff("staff.txt", oldStaff, newStaff);
 				break;
 			}
@@ -264,8 +244,7 @@ public class AdminController extends EmployeeController {
 				String branch;
 				System.out.println("Select a branch: ");
 				List<Branch> al = BranchTextDB.readBranchList("branch.txt");// test
-				for (Branch b : al)
-				{
+				for (Branch b : al) {
 					System.out.println(i + ") " + b.getName());
 					i++;
 				}
@@ -303,8 +282,8 @@ public class AdminController extends EmployeeController {
 				StaffDisplay.printStaffList(selection);
 				break;
 			}
-			
-			case(5):
+
+			case (5):
 				StaffTextDB.printStaffList("staff.txt");
 				break;
 			default:
@@ -462,68 +441,59 @@ public class AdminController extends EmployeeController {
 		return;
 	}
 
-	public void addBranch() throws IOException
-	{
-		
+	public void addBranch() throws IOException {
+
 		System.out.println("Name:");
 		String name = sc.nextLine();
-		
-		
-		List<Branch> al = BranchTextDB.readBranchList("branch.txt");//test
-        for (Branch branch : al)
-        {
-        	if (branch.getName().equalsIgnoreCase(name))
-        	{
-        		System.out.println("Branch already exists");
-        		return;
-        	}
-        }
-       //String name, String location, int staffQuota, BranchStatus branchStatus
+
+		List<Branch> al = BranchTextDB.readBranchList("branch.txt");// test
+		for (Branch branch : al) {
+			if (branch.getName().equalsIgnoreCase(name)) {
+				System.out.println("Branch already exists");
+				return;
+			}
+		}
+		// String name, String location, int staffQuota, BranchStatus branchStatus
 		System.out.println("Location");
 		String location = sc.nextLine();
 		System.out.println("Staff Quota:");
 		int staffQuota = sc.nextInt();
-		sc.nextLine(); //input buffer
-		
-		//assumption that every new branch added will be set as open
-		
+		sc.nextLine(); // input buffer
+
+		// assumption that every new branch added will be set as open
+
 		System.out.println("Branch has been succesfully added");
-	
-		Branch branch = new Branch(name, location , staffQuota, BranchStatus.OPEN);
+
+		Branch branch = new Branch(name, location, staffQuota, BranchStatus.OPEN);
 		BranchTextDB.addBranch("branch.txt", branch);
-		
-		
+
 	}
 
-	public void removeBranch() throws IOException
-	{
+	public void removeBranch() throws IOException {
 		System.out.println("=======Branch List=======");
-		List<Branch> al = BranchTextDB.readBranchList("branch.txt");//test
-		
-		for (Branch branch: al)
-        {
-        	System.out.println(branch.getName());
-        }
+		List<Branch> al = BranchTextDB.readBranchList("branch.txt");// test
+
+		for (Branch branch : al) {
+			System.out.println(branch.getName());
+		}
 		// BranchTextDB.printBranch("branch.txt");
-		
+
 		System.out.println("Enter Branch Name:");
 		String name = sc.nextLine();
-		
+
 		Branch toRemove = null;
-        for (Branch branch: al)
-        {
-        	if (branch.getName().equals(name)) // assumptions that branch names are always unique
-        	{
-        		toRemove = branch;
-        		BranchTextDB.removeBranch("branch.txt", toRemove);
-        		System.out.println("Branch has been removed successfully.");
-        		break;
-        	}
-        }
-        if (toRemove == null)
-        {
-        	System.out.println("Branch does not exist");
-        }
+		for (Branch branch : al) {
+			if (branch.getName().equals(name)) // assumptions that branch names are always unique
+			{
+				toRemove = branch;
+				BranchTextDB.removeBranch("branch.txt", toRemove);
+				System.out.println("Branch has been removed successfully.");
+				break;
+			}
+		}
+		if (toRemove == null) {
+			System.out.println("Branch does not exist");
+		}
 	}
 
 }
